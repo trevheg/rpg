4b9b77a843fc85606f2898497ead6a8acf5a5fb0
trevheg <trevorhegstrom@gmail.com>
2025-12-15 20:28:39 -0800

    fixed bug where the menu js wasn't finding the menu in header.html

--- scripts/header.js
+++ scripts/header.js
@@ -3,6 +3,20 @@ function loadHeader() {
         .then(response => response.text())
         .then(data => {
             document.querySelector("header").innerHTML = data;
+
+            const menuButton = document.querySelector("#menu-button");
+            const aniNav = document.querySelector("nav")
+            const navigation = document.querySelector("#nav-list");
+
+            if (menuButton) {
+                menuButton.addEventListener("click", () => {
+                    aniNav.classList.toggle("open");
+                    navigation.classList.toggle("open");
+                    menuButton.classList.toggle("open");
+                })                
+            }
+
+
         })
         .catch(error => console.error("error loading header". error));
     
